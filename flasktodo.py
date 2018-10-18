616e563631c8f9b4e5f6554b3319a6b9c024e24e
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-18 14:18:40 +0000

    to do items rendered

--- flasktodo.py
+++ flasktodo.py
@@ -3,15 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 tasks = {
-	1 : {
-		"id: 1,
+	1: {
 		"name": "get coffee",
 		"description": "none of that cheap stuff",
 		"is_urgent": False
 	},
-	
-	2 : {
-		"id": 2,
+	2: {
 		"name": "get milk",
 		"description": "in a carton",
 		"is_urgent": True
@@ -19,20 +16,11 @@ tasks = {
 }
 
 
-task_to_add = {
-		"id": next_id,
-		"name": request.form['name'],
-		"description": request...,
-		"is_urgent": True
-	}
-
-tasks[????] = task_to_add
-
 
 
 @app.route("/")
-def show_hi():
-    return "Hi"
+def todo():
+    return render_template("index.html", tasks=tasks.values())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
