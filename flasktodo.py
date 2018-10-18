333402acfec03db4fc23089435e5817e73f45f6a
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-18 13:43:59 +0000

    data structure added

--- flasktodo.py
+++ flasktodo.py
@@ -2,6 +2,34 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = {
+	1 : {
+		"id: 1,
+		"name": "get coffee",
+		"description": "none of that cheap stuff",
+		"is_urgent": False
+	},
+	
+	2 : {
+		"id": 2,
+		"name": "get milk",
+		"description": "in a carton",
+		"is_urgent": True
+	},
+}
+
+
+task_to_add = {
+		"id": next_id,
+		"name": request.form['name'],
+		"description": request...,
+		"is_urgent": True
+	}
+
+tasks[????] = task_to_add
+
+
+
 @app.route("/")
 def show_hi():
     return "Hi"
