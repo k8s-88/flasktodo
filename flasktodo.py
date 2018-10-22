f817ea608b07eb86ba6c39929f05bb744d9b8fe2
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-22 08:29:57 +0000

    final commit

--- flasktodo.py
+++ flasktodo.py
@@ -69,4 +69,9 @@ def edit_task(id):
     
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080, debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+    
+    
+    
+    
+    
