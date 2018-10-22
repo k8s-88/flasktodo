337f8ed6b409f997a37b4478e212cb7dd1d28905
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-22 08:17:25 +0000

    prepare for heroku deploy

--- flasktodo.py
+++ flasktodo.py
@@ -1,26 +1,72 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, redirect
+import os
 
 app = Flask(__name__)
 
 tasks = {
 	1: {
+	    "id": 1,
 		"name": "get coffee",
 		"description": "none of that cheap stuff",
-		"is_urgent": False
+		"urgent": False,
+		"done":	True
 	},
 	2: {
+	    "id": 2,
 		"name": "get milk",
 		"description": "in a carton",
-		"is_urgent": True
+		"urgent": True,
+		"done": False
 	},
 }
 
+next_id = 3
 
 
 
 @app.route("/")
-def todo():
+def show_index():
     return render_template("index.html", tasks=tasks.values())
+    
+    
+@app.route("/add", methods=["GET", "POST"])
+def show_add_form():
+    if request.method == "POST":
+    	global next_id
+    	new_item = {
+    		"id": next_id,
+    		"name": request.form["name"],
+    		"description": request.form["description"],
+    		"urgent": "urgent" in request.form,
+    		"done": "done" in request.form
+    		
+    	}
+    	print(new_item)
+    	tasks[next_id] = new_item
+    	next_id += 1
+    	return redirect("/")
+    
+    else:
+        
+        return render_template("todo_form.html")
+    
+    
+@app.route("/edit/<int:id>", methods=["GET", "POST"])   
+def edit_task(id):
+	if request.method == "POST":
+		changed_item = {
+			"id": id,
+			"name": request.form["name"],
+			"description": request.form["description"],
+			"urgent": "urgent" in request.form,
+			"done": "done" in request.form
+			
+		}
+		tasks[id] = changed_item
+		return redirect("/")
+	else:
+		return render_template("edit_form.html", task = tasks[id])
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
